d647c46c240e2133efd0d6403216bbb3c1bb3b9b
mirumi <info@milmemo.net>
2022-06-29 22:11:12 +0900

    add: route53 resources

---
+++ modules/virginia/route53.tf
@@ -0,0 +1,32 @@
+resource "aws_route53_zone" "kei_ooo" {
+  name  = "kei.ooo"
+  count = var.env_name == "prd" ? 1 : 0
+
+  tags = var.tags
+}
+
+resource "aws_route53_record" "kei_ooo_A" {
+  zone_id = aws_route53_zone.kei_ooo[count.index].zone_id
+  name    = "kei.ooo"
+  type    = "A"
+
+  alias {
+    name                   = "d2i1p1l37l2o59.cloudfront.net."
+    zone_id                = "Z2FDTNDATAQYW2"  # https://bit.ly/3HV6NFX
+    evaluate_target_health = false
+  }
+
+  count = var.env_name == "prd" ? 1 : 0
+}
+
+resource "aws_route53_record" "kei_ooo_ACM" {
+  zone_id = aws_route53_zone.kei_ooo[count.index].zone_id
+  name    = tolist(aws_acm_certificate.kei_ooo.domain_validation_options)[0]["resource_record_name"]  # https://bit.ly/3brCgDf
+  records = [tolist(aws_acm_certificate.kei_ooo.domain_validation_options)[0]["resource_record_value"]]
+  type    = "CNAME"
+  ttl     = 3600
+  
+  allow_overwrite = true
+
+  count = var.env_name == "prd" ? 1 : 0
+}
