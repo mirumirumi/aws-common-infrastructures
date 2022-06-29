743a07c6f6dbe88d5a4fce9664245095a6ebb7fd
mirumi <info@milmemo.net>
2022-06-29 22:15:28 +0900

    fix: suppressed s3 acl deprecated warning

--- modules/common/s3.tf
+++ modules/common/s3.tf
@@ -1,13 +1,21 @@
 resource "aws_s3_bucket" "tfstate" {
   bucket = "${var.resource_prefix}-${var.env_name}-tfstate-artifactstore"
-  acl    = "private"
 
   tags = var.tags
 }
 
+resource "aws_s3_bucket_acl" "tfstate" {
+  bucket = "${var.resource_prefix}-${var.env_name}-tfstate-artifactstore"
+  acl    = "private"
+}
+
 resource "aws_s3_bucket" "sam-artifactstore" {
   bucket = "${var.resource_prefix}-${var.env_name}-sam-artifactstore"
-  acl    = "private"
 
   tags = var.tags
+}
+
+resource "aws_s3_bucket_acl" "sam-artifactstore" {
+  bucket = "${var.resource_prefix}-${var.env_name}-sam-artifactstore"
+  acl    = "private"
 }
