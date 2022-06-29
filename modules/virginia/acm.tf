741e98fba6ebbb372a6f3b67b7941625c5c3ad89
mirumi <info@milmemo.net>
2022-06-29 22:59:27 +0900

    add: mirumi.tech domain

--- modules/virginia/acm.tf
+++ modules/virginia/acm.tf
@@ -1,3 +1,21 @@
+resource "aws_acm_certificate" "mirumi_tech" {
+  provider = aws.useast1
+
+  domain_name       = "mirumi.tech"
+  validation_method = "DNS"
+
+  tags = var.tags
+}
+
+resource "aws_acm_certificate_validation" "mirumi_tech" {
+  provider = aws.useast1
+
+  certificate_arn         = aws_acm_certificate.mirumi_tech.arn
+  validation_record_fqdns = [for record in aws_route53_record.mirumi_tech_ACM : record.fqdn]
+}
+
+
+
 resource "aws_acm_certificate" "kei_ooo" {
   provider = aws.useast1
 
--- modules/virginia/route53.tf
+++ modules/virginia/route53.tf
@@ -1,3 +1,38 @@
+resource "aws_route53_zone" "mirumi_tech" {
+  name  = "mirumi.tech"
+  count = var.env_name == "prd" ? 1 : 0
+
+  tags = var.tags
+}
+
+resource "aws_route53_record" "mirumi_tech_A" {
+  zone_id = aws_route53_zone.mirumi_tech[count.index].zone_id
+  name    = "mirumi.tech"
+  type    = "A"
+
+  alias {
+    name                   = "xxxxxxxxxxxxx.cloudfront.net."
+    zone_id                = "Z2FDTNDATAQYW2"  # https://bit.ly/3HV6NFX
+    evaluate_target_health = false
+  }
+
+  count = var.env_name == "prd" ? 1 : 0
+}
+
+resource "aws_route53_record" "mirumi_tech_ACM" {
+  zone_id = aws_route53_zone.mirumi_tech[count.index].zone_id
+  name    = tolist(aws_acm_certificate.mirumi_tech.domain_validation_options)[0]["resource_record_name"]  # https://bit.ly/3brCgDf
+  records = [tolist(aws_acm_certificate.mirumi_tech.domain_validation_options)[0]["resource_record_value"]]
+  type    = "CNAME"
+  ttl     = 3600
+  
+  allow_overwrite = true
+
+  count = var.env_name == "prd" ? 1 : 0
+}
+
+
+
 resource "aws_route53_zone" "kei_ooo" {
   name  = "kei.ooo"
   count = var.env_name == "prd" ? 1 : 0
